74d13867451380aba1c570856af71c3f39a0a9a8
Fireman <kamils1234567@gmail.com>
2022-06-01 20:39:27 +0200

    fix#4

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 #define feedback_pin_with_interrupt_5 4
 int fulfilmentR, fulfilmentG, fulfilmentB = 0;
 uint8_t savedBrightness;
-int counter;
 void RGB(uint8_t, uint8_t, uint8_t, uint8_t);
 void thunderFunction();
 void setup()
@@ -19,47 +18,25 @@ void setup()
 
 void loop()
 {
-  if (counter > 0)
-  {
-    fulfilmentR = 255;
-    fulfilmentG = 255;
-    fulfilmentB = 255;
-    RGB(255, 255, 255, 255);
-    delay(100);
-    for (size_t f = 255; f >= 0; f--)
-    {
-      RGB(f, 255, 255, f);
-      delay(10);
-      if (f == 0)
-      {
-        return;
-      }
-    }
-  }
-  counter--;
 }
 
 void thunderFunction()
 {
-  counter++;
+  fulfilmentR = 255;
+  fulfilmentG = 255;
+  fulfilmentB = 255;
+  RGB(255, 255, 255, 255);
+  delay(100);
+  for (size_t f = 255; f >= 0; f--)
+  {
+    if (f == 0)
+    {
+      return;
+    }
+    RGB(f, 255, 255, f);
+    delay(10);
+  }
 }
-// void thunderFunction()
-// {
-//   fulfilmentR = 255;
-//   fulfilmentG = 255;
-//   fulfilmentB = 255;
-//   RGB(255, 255, 255, 255);
-//   delay(100);
-//   for (size_t f = 255; f >= 0; f--)
-//   {
-//     RGB(f, 255, 255, f);
-//     delay(10);
-//     if (f == 0)
-//     {
-//       return;
-//     }
-//   }
-// }
 
 void RGB(uint8_t r, uint8_t g, uint8_t b, uint8_t brightness)
 {
