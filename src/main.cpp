fb51ed42c8caef9690dd6bc3dcf62286a5dd192d
Fireman <kamils1234567@gmail.com>
2022-06-01 20:32:44 +0200

    fix#3

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,13 @@
 #define feedback_pin_with_interrupt_5 4
 int fulfilmentR, fulfilmentG, fulfilmentB = 0;
 uint8_t savedBrightness;
+int counter;
 void RGB(uint8_t, uint8_t, uint8_t, uint8_t);
 void thunderFunction();
 void setup()
 {
   pinMode(feedback_pin_with_interrupt_5, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(feedback_pin_with_interrupt_5), thunderFunction, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(feedback_pin_with_interrupt_5), thunderFunction, FALLING);
   pinMode(Rpin, OUTPUT);
   pinMode(Gpin, OUTPUT);
   pinMode(Bpin, OUTPUT);
@@ -18,25 +19,47 @@ void setup()
 
 void loop()
 {
+  if (counter > 0)
+  {
+    fulfilmentR = 255;
+    fulfilmentG = 255;
+    fulfilmentB = 255;
+    RGB(255, 255, 255, 255);
+    delay(100);
+    for (size_t f = 255; f >= 0; f--)
+    {
+      RGB(f, 255, 255, f);
+      delay(10);
+      if (f == 0)
+      {
+        return;
+      }
+    }
+  }
+  counter--;
 }
 
 void thunderFunction()
 {
-  fulfilmentR = 255;
-  fulfilmentG = 255;
-  fulfilmentB = 255;
-  RGB(255, 255, 255, 255);
-  delay(100);
-  for (size_t f = 255; f >= 0; f--)
-  {
-    RGB(f, 255, 255, f);
-    delay(10);
-    if (f == 0)
-    {
-      return;
-    }
-  }
+  counter++;
 }
+// void thunderFunction()
+// {
+//   fulfilmentR = 255;
+//   fulfilmentG = 255;
+//   fulfilmentB = 255;
+//   RGB(255, 255, 255, 255);
+//   delay(100);
+//   for (size_t f = 255; f >= 0; f--)
+//   {
+//     RGB(f, 255, 255, f);
+//     delay(10);
+//     if (f == 0)
+//     {
+//       return;
+//     }
+//   }
+// }
 
 void RGB(uint8_t r, uint8_t g, uint8_t b, uint8_t brightness)
 {
